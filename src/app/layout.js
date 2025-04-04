fc574fe60b0c17bef42a268a3453c235da00c923
AnnaFutryk <anna.futryk@gmail.com>
2025-04-04 11:27:29 +0300

    create project

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,32 @@
-import { Montserrat } from "next/font/google";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+import { Raleway } from "next/font/google";
+
 import "./globals.css";
+import { Providers } from "../../providers";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export const metadata = {
-  title: "Cyanidium Dev. Template",
-  description: "NextJS NextUI template",
+  title: "Твое спасение",
+  description: "Сила любви у тебя в руках",
   icons: {
     icon: "/icons/favicon.ico",
   },
 };
 
-const montserrat = Montserrat({
+const raleway = Raleway({
   subsets: ["latin"],
-  weight: ["400", "700"],
+  weight: ["400", "500", "600", "700", "800"],
 });
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${montserrat.className}`}>
+    <html lang="ru" className={`${raleway.className}`}>
       <body>
-        <Header />
-        {children}
-        <Footer />
+        <Providers>
+          <Header />
+          <main>{children}</main>
+          <Footer />
+        </Providers>
       </body>
     </html>
   );
